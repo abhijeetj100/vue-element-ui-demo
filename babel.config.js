6be5efdd15656ca42b2d9538af957ce252c2a232
Abhijeet Jain <abhijeetjain456@gmail.com>
2022-09-17 03:05:00 +0530

    Feat(): Dark theme in Element-UI

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,8 @@ module.exports = {
       "component",
       {
         libraryName: "element-ui",
-        styleLibraryName: "theme-chalk",
+        // styleLibraryName: "theme-chalk",
+        styleLibraryName: "~src/theme/dark/lib",
       },
     ],
   ],
